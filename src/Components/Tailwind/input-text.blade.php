96c9232401efbaf240814d5bfbccbe4f18b0e753
Cirote <estebanrogel@gmail.com>
2022-02-12 09:45:59 -0500

    Box

--- src/Components/Tailwind/input-text.blade.php
+++ src/Components/Tailwind/input-text.blade.php
@@ -1,14 +1,10 @@
-<div class="form-group @if($errors->has($attributes->whereStartsWith('wire:model')->first())) has-error @endif">
-    <label for="entrada" class="control-label">
-        {{ $slot }}
-    </label>
-    <input {{ $attributes->merge([
-        'class' => 'form-control',
-        'type'  => 'text'
-    ]) }} id="entrada" placeholder="@lang('entradas.n_entrada_pl')" >
-    @if($errors->has($attributes->whereStartsWith('wire:model')->first()))
-        @foreach ($errors->get($attributes->whereStartsWith('wire:model')->first()) as $message)
-            <span class="help-block">* {{ $message }}</span>
-        @endforeach
-    @endif
-</div>
+@props(['disabled' => false])
+
+@props(['value'])
+
+<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }}>
+    {{ $value ?? $slot }}
+</label>
+
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm']) !!}>
+
