0a18c2b167bb26f79dfeea314fb5de57858d540f
Esteban Rogel <estebanrogel@gmail.com>
2024-08-09 15:50:45 -0400

    Refactor

--- Components/AdminLTE3/tarjeta.blade.php
+++ Components/AdminLTE3/tarjeta.blade.php
@@ -2,11 +2,14 @@
     <div class="small-box">
         <div class="inner">
             <h3>{{ $slot }}</h3>
-            <p>{{ $title ?? '' }}</p>
+            {{ $title ?? '' }}
         </div>
         <a href="{{ $href ?? '#' }}" class="small-box-footer bg-{{ $bgColor ?? 'default' }}">
-            <span class="text-muted">
-                {{ $footer ?? '&nbsp;' }}
+            <span class="{{ isset($bgColor) ? '' : 'text-muted' }}">
+                @isset($footer)
+                    {{ $footer }}
+                @endisset
+                &nbsp;
                 @isset($icon)
                     <i class="{{ $icon }}"></i>
                 @endisset
