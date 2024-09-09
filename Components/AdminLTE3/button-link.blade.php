c6483af72bfc3fa8e965e7ccc9ba1e5f15f5d869
Esteban <estebanrogel@gmail.com>
2024-09-09 17:00:37 -0300

    Boton

--- Components/AdminLTE3/button-link.blade.php
+++ Components/AdminLTE3/button-link.blade.php
@@ -5,5 +5,6 @@
 ])
 
 <a href="{{ $href }}" {{ $attributes->merge(['class' => "btn btn-{$type} btn-sm"]) }} > 
-<i class="{{ $icon }}"></i> {{ $slot }}
+    <i class="{{ $icon }}"></i> {{ $slot }}
 </a>
+
