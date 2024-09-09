4704ba7751694ac749344d6ebddefc049c2d992c
Esteban <estebanrogel@gmail.com>
2024-09-09 16:52:37 -0300

    Button link

---
+++ Components/AdminLTE3/button-link.blade.php
@@ -0,0 +1,9 @@
+@props([
+    'href' => '#',
+    'type' => 'info',
+    'icon' => 'fa-solid fa-shoe-prints',
+])
+
+<a href="{{ $href }}" {{ $attributes->merge(['class' => "btn btn-{$type} btn-sm"]) }} > 
+<i class="{{ $icon }}"></i> {{ $slot }}
+</a>
